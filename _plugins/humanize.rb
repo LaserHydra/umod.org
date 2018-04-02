27e70e755c6ca65352da71889717c884a556bfa7
Luke Spragg <luke@spragg.me>
2018-04-02 01:46:48 -0400

    Add exclusions to humanize formatting plugin

--- _plugins/humanize.rb
+++ _plugins/humanize.rb
@@ -1,17 +1,22 @@
 class String
   def humanize
-    # Split PascalCase on upper cases after a lowercase character (ex. WelcomeTP)
-    self.gsub(/([[:lower:]\\d])([[:upper:]])/, '\1 \2') \
-    # Split PascalCase on first capital after two or more capitals (ex. NPCManager)
-    .gsub(/([[:upper:]]{2,})([[:upper:]][[:lower:]\\d])/,'\1 \2') \
-    # Split before a number with multiple characters in front of it (ex. Test99Tool)
-    .gsub(/(\D{2,})(\d+)/, '\1 \2') \
-    # Split PascalCase after a number (ex. C4Logger)
+    # TODO: Add support for other string formats and small words (ie. and, to, or, of)
+
+    # List of abbreviations and/or words to exclude from formatting
+    exclusions = [ "IPv4", "IPv6", "MySQL" ]
+
+    # Split at upper case letter prefixed by an excluded word                (ex. MySQLWhitelist)
+    self.gsub(/(#{exclusions.join('|')})([[:upper:]])/, '\1 \2') \
+    # Split at upper case letter prefixed by a lower case letter             (ex. WelcomeTP or Tele2Player)
+    .gsub(/(!(#{exclusions.join('|')}))([[:lower:]\\d])([[:upper:]])/, '\1 \2') \
+    # Split at upper case letter prefixed by two or more upper case letters  (ex. NPCManager or NPCKitC4)
+    .gsub(/([[:upper:]]{2,})([[:upper:]][[:lower:]\\d])/, '\1 \2') \
+    # Split at upper case letter prefixed by two or more numbers             (ex. Epic99Arena)
+    .gsub(/(\d{2,})(\d+)/, '\1 \2') \
+    # Split at upper case letter prefixed by a number                        (ex. C4Logger)
     .gsub(/(\d)/, '\1 \2') \
     # Remove any whitespace from start or end of string
     .strip
-
-    # TODO: Add support for other string formats and small words (ie. and, to, or, of)
   end
 end
 
